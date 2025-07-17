b9827d0774dfc27ca4fa1e1b1188bf9de39a9783
Mara Clara Aquino <mariaclaraaquino04@gmail.com>
2025-07-17 10:14:20 -0300

    Update perfilUsuario_routes.js

--- Projeto atualizado/src/routes/perfilUsuario_routes.js
+++ Projeto atualizado/src/routes/perfilUsuario_routes.js
@@ -1,14 +1,9 @@
 import express from 'express';
-import {
-  vincularPerfil,
-  listarVinculos,
-  desvincularPerfil
-} from '../controllers/perfilUsuario_controller.js';
+import { atribuirPerfilAoUsuario, removerPerfil } from '../controllers/perfilUsuario_controller.js';
 
 const router = express.Router();
 
-router.post('/', vincularPerfil);
-router.get('/', listarVinculos);
-router.delete('/:id_usuario/:id_perfil', desvincularPerfil);
+
+router.delete('/:id_usuario/remove-perfil', removerPerfil);
 
 export default router;
