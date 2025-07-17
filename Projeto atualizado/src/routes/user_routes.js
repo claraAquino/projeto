138f0513093d762ca2dd8c9af7a387a5bae0413a
Mara Clara Aquino <mariaclaraaquino04@gmail.com>
2025-07-17 10:16:20 -0300

    Update user_routes.js

--- Projeto atualizado/src/routes/user_routes.js
+++ Projeto atualizado/src/routes/user_routes.js
@@ -1,10 +1,29 @@
-import { Router } from 'express';
-import { createUser, updateUser, deleteUser } from '../controllers/user_controller.js';
+import express from 'express';
+import { 
+  createUserByAdm, 
+  updateUser, 
+  deleteUser, 
+  atribuirPerfilAoUsuario, 
+  getUsers, 
+  loginUser, 
+  getUsuarioPorEmail,
+  toggleStatusUsuario  // vamos criar essa função no controller
+} from '../controllers/user_controller.js';
 
-const router = Router();
-//ROTAS DO CRUD
-router.post('/', createUser);      // Criar usuário
-router.put('/:id', updateUser);    // Atualizar usuário
-router.delete('/:id', deleteUser); // Excluir usuário
+import { autenticar } from '../middlewares/auth.js';
+const router = express.Router();
+
+router.post('/', createUserByAdm);          // criação via admin
+router.put('/:id', updateUser);             // Atualizar usuário
+router.delete('/:id', deleteUser);          // Excluir usuário
+router.get('/', getUsers);                   // Listar usuários
+
+router.put('/:id_usuario/atribuir-perfil', atribuirPerfilAoUsuario);
+
+//rota para toggle status ativo/inativo
+router.patch('/:id_usuario/status', autenticar, toggleStatusUsuario);
+
+router.get('/email/:email', autenticar, getUsuarioPorEmail);
+router.post('/login', loginUser);           // Login
 
 export default router;
