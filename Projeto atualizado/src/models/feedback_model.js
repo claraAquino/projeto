0a12c167f7a8d60f0893435637ae3f77b044ab73
Mara Clara Aquino <mariaclaraaquino04@gmail.com>
2025-07-28 18:07:23 -0300

    Update feedback_model.js

--- Projeto atualizado/src/models/feedback_model.js
+++ Projeto atualizado/src/models/feedback_model.js
@@ -1,13 +1,20 @@
 import { DataTypes } from 'sequelize';
 import { sequelize } from '../config/database.js';
+import { Sugestao } from './sugestao_model.js';
 
 export const Feedback = sequelize.define('Feedback', {
   id_feedback:  { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, field: 'id_feedback' },
-  id_consulta:  { type: DataTypes.INTEGER, allowNull: false, unique: true, field: 'id_consulta' },
+  id_sugestao:  { 
+    type: DataTypes.INTEGER, 
+    allowNull: false,
+    field: 'id_sugestao',
+    unique: true, 
+    references: { model: Sugestao, key: 'id_sugestao' }
+  },
   util:         { type: DataTypes.BOOLEAN, allowNull: false, field: 'util' },
   data_feedback:{ type: DataTypes.DATE, defaultValue: DataTypes.NOW, field: 'data_feedback' }
 }, {
   tableName: 'feedback',
-  schema:    'queroquero',
+  schema:    'schema',
   timestamps: false
 });
