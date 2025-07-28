7f85e149dcdec91aa35166b0689b324f3ba703a3
Mara Clara Aquino <mariaclaraaquino04@gmail.com>
2025-07-28 18:05:22 -0300

    Update consulta_model.js

--- Projeto atualizado/src/models/consulta_model.js
+++ Projeto atualizado/src/models/consulta_model.js
@@ -1,13 +1,30 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/database.js';
+import {DataTypes} from 'sequelize';
+import {sequelize} from '../config/database.js';
+
 
 export const Consulta = sequelize.define('Consulta', {
-  id_consulta:  { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, field: 'id_consulta' },
-  id_sessao:    { type: DataTypes.INTEGER, allowNull: false, field: 'id_sessao' },
-  input:        { type: DataTypes.TEXT, allowNull: false, field: 'input' },
-  data_consulta:{ type: DataTypes.DATE, defaultValue: DataTypes.NOW, field: 'data_consulta' }
+  id_consulta:  { 
+    type: DataTypes.INTEGER, 
+    primaryKey: true, 
+    autoIncrement: true, 
+    field: 'id_consulta' 
+  },
+  id_sessao:    { 
+    type: DataTypes.INTEGER, 
+    allowNull: false, 
+    field: 'id_sessao' 
+  },
+  input:        { 
+    type: DataTypes.TEXT, 
+    allowNull: false, 
+    field: 'input' 
+  },
+  data_consulta:{ 
+    type: DataTypes.DATE, 
+    defaultValue: DataTypes.NOW, 
+    field: 'data_consulta' }
 }, {
   tableName: 'consulta',
-  schema:    'queroquero',
+  schema:    'schema',
   timestamps: false
 });
