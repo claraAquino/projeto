f5976e4ee351081f5fc69ebc45f5581990d1f4b2
Mara Clara Aquino <mariaclaraaquino04@gmail.com>
2025-07-28 18:10:52 -0300

    Update solucaonaoencontrada_model.js

--- Projeto atualizado/src/models/solucaonaoencontrada_model.js
+++ Projeto atualizado/src/models/solucaonaoencontrada_model.js
@@ -2,13 +2,13 @@ import { DataTypes } from 'sequelize';
 import { sequelize } from '../config/database.js';
 
 export const SolucaoNaoEncontrada = sequelize.define('SolucaoNaoEncontrada', {
-  id_solucaonaoencontrada:{ type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, field: 'id_solucaonaoencontrada' },
-  data_criacao:           { type: DataTypes.DATE, defaultValue: DataTypes.NOW, field: 'data_criacao' },
-  id_consulta:            { type: DataTypes.INTEGER, allowNull: false, field: 'id_consulta' },
-  input:                  { type: DataTypes.TEXT, allowNull: false, field: 'input' },
-  status:                 { type: DataTypes.STRING(20), allowNull: false, field: 'status' }
+  id_solucaonaoencontrada: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, field: 'id_solucaonaoencontrada' },
+  data_criacao:            { type: DataTypes.DATE, defaultValue: DataTypes.NOW, field: 'data_criacao' },
+  id_consulta:             { type: DataTypes.INTEGER, allowNull: false, field: 'id_consulta' },
+  input:                   { type: DataTypes.TEXT, allowNull: false, field: 'input' },
+  statusdoc:               { type: DataTypes.STRING(20), allowNull: false, field: 'statusdoc' }
 }, {
   tableName: 'solucaonaoencontrada',
-  schema:    'queroquero',
+  schema: 'schema',
   timestamps: false
 });
