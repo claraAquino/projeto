22ee26dfa9fdcc1746cbc326d83f567ce76b1b41
Mara Clara Aquino <mariaclaraaquino04@gmail.com>
2025-07-28 18:11:36 -0300

    Update sugestao_model.js

--- Projeto atualizado/src/models/sugestao_model.js
+++ Projeto atualizado/src/models/sugestao_model.js
@@ -1,14 +1,45 @@
 import { DataTypes } from 'sequelize';
-import { sequelize } from '../config/database.js'; 
+import { sequelize } from '../config/database.js';
 
 export const Sugestao = sequelize.define('Sugestao', {
-  id_sugestao: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, field: 'id_sugestao' },
-  id_consulta: { type: DataTypes.INTEGER, allowNull: false, unique: true, field: 'id_consulta' },
-  solucao:     { type: DataTypes.TEXT, allowNull: false, field: 'solucao' },
-  id_documento:{ type: DataTypes.INTEGER, field: 'id_documento' },
-  data_sugestao:{ type: DataTypes.DATE, defaultValue: DataTypes.NOW, field: 'data_sugestao' }
+  id_sugestao: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    field: 'id_sugestao'
+  },
+  id_consulta: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'id_consulta'
+  },
+  id_documento: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    field: 'id_documento'
+  },
+  solucao: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    field: 'solucao'
+  },
+  data_sugestao: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+    field: 'data_sugestao'
+  },
+  avaliacoes_positivas: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+    field: 'avaliacoes_positivas'
+  },
+  avaliacoes_negativas: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+    field: 'avaliacoes_negativas'
+  }
 }, {
   tableName: 'sugestao',
-  schema:    'queroquero',
+  schema: 'schema',
   timestamps: false
 });
